e0dd8fa1f8bd26efeed0973d2bd278fbedaf06da
Will-S <willschrick@gmail.com>
2025-02-25 09:17:31 -0800

    one song created..more to do

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,7 +2,7 @@
 //import reactLogo from './assets/react.svg';
 //import viteLogo from '/vite.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from '../routes/HomePage.jsx';
+import HomePage from '../routes/HomeP.jsx';
 import SongsPage from '../routes/SongsPage.jsx';
 import AddSongPage from '../routes/AddSongPage.jsx';
 import '../App.css';
