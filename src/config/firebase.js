1416915f54ae5307265bfba197a8623dd1342df2
Will-S <willschrick@gmail.com>
2025-03-14 11:44:40 -0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getRemoteConfig, fetchAndActivate } from 'firebase/remote-config';
 
 const firebaseConfig = {
-  apiKey: '',
+  apiKey: 'AIzaSyBp9a0BfdOrwqzgVUFQHrFB7z-1oWYJiOs',
   authDomain: 'react-firestore-exercise.firebaseapp.com',
   projectId: 'react-firestore-exercise-4ae95',
   appId: '491623188913',
